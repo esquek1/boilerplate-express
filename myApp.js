c1bf56a66c9a3a5143776a64e15e8590f8a408ff
Kelly Esquejo <esquejokelly@gmail.com>
2024-10-29 17:38:19 +0000

    Challenge 4 - Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -16,27 +16,22 @@ let app = express();
 // })
 
 // Challenge 3 - Serve an HTML File
-/*You can respond to requests with a file using the res.sendFile(path) 
-method. You can put it inside the app.get('/', ...) route handler. 
-Behind the scenes, this method will set the appropriate headers to instruct 
-your browser on how to handle the file you want to send, according to its 
-type. Then it will read and send the file. This method needs an absolute 
-file path. We recommend you to use the Node global variable __dirname to 
-calculate the path like this:
-
-Send the /views/index.html file as a response to GET requests to the / 
-path. If you view your live app, you should see a big HTML heading (and a 
-form that we will use later…), with no style applied.
-*/
-
+// https://www.freecodecamp.org/learn/back-end-development-and-apis/basic-node-and-express/serve-an-html-file
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 })
 
+// Challenge 4 - Serve Static Assets
+app.use("/public", express.static(__dirname + "/public"))
+
+
+// Challenge 5 - Serve JSON on a Specific Route
+//https://www.freecodecamp.org/learn/back-end-development-and-apis/basic-node-and-express/serve-json-on-a-specific-route
+
+ 
 
 
 
 
 
-
- module.exports = app;
+module.exports = app;
