cb3a4ed8ebaf34c18c0f83b817cd6a213a9aa1f7
Kelly Esquejo <esquejokelly@gmail.com>
2024-10-29 20:59:40 +0000

    Challenge - Use the .env File

--- myApp.js
+++ myApp.js
@@ -1,3 +1,6 @@
+// Challenge 5 - dotenv package to load environment variables from the .env 
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
@@ -28,13 +31,18 @@ app.use("/public", express.static(__dirname + "/public"))
 // Challenge 5 - Serve JSON on a Specific Route
 // https://www.freecodecamp.org/learn/back-end-development-and-apis/basic-node-and-express/serve-json-on-a-specific-route
 // Create a simple API by creating a route that responds with JSON at the path "/json"
-app.get("/json", (req, res) => {
-    res.json({"message": "Hello json"});
-})
+// app.get("/json", (req, res) => {
+    
+//     res.json({"message": "HELLO json"});
+// })
  
-
-
-
-
+// Challenge 6- Use the .env File
+// https://www.freecodecamp.org/learn/back-end-development-and-apis/basic-node-and-express/use-the--env-file
+// Create .env file and add dotenv package 
+app.get("/json", (req, res) => {
+    res.json({
+        "message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"
+    })
+})
 
 module.exports = app;
