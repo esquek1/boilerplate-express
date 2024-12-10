d2bdc86a62ca49e82ccd19aa0ca58c5db9f0fb16
Kelly Esquejo <esquejokelly@gmail.com>
2024-12-10 20:45:25 +0000

    Completed challenge #7

--- myApp.js
+++ myApp.js
@@ -56,5 +56,43 @@ app.get("/json", (req, res) => {
     })
 })
 
+// Challenge 7 - Implement a Root-Level Request Logger Middleware 
+//https://www.freecodecamp.org/learn/back-end-development-and-apis/basic-node-and-express/implement-a-root-level-request-logger-middleware
+/*
+Middleware functions 
+- functions that take 3 arguments: 
+(request obj, response obj, next function in the apps request-response cycle)
+- execute some code that:
+(adds info to the req/res obj)
+or (end the cycle by sending a res when a condition is met)
+
+If no response is sent when they are done, 
+they start execution of the next function in the stack
+(triggers calling the 3rd arg, next() )
+*/
+
+/* 
+function(req, res, next) {
+  console.log("I'm a middleware...");
+  next();
+}
+*/
+
+//TO MOUNT A MIDDLEWARE FUNCTION AT ROOT LEVEL
+// app.use(<mware-function>) method
+// (function will be executed for all reqs - can specify conditions)
+
+// ex: if you want function to be executed only for POST req, use
+// app.post(<mware-function>)
+
+
+//TODO: build a simple logger
+// For every req, it should log to console
+// format: method path - ip
+// Ex: GET /json - ::ffff:127:0.0.1
+
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+}) 
 
 module.exports = app;
